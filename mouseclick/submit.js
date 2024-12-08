bedf87016b3508ba18589375976991097ae10dce
puspatharu <t.rajesh2077@gmail.com>
2024-12-08 19:39:07 +0545

    initial commit

---
+++ mouseclick/submit.js
@@ -0,0 +1,28 @@
+
+
+function clickFigure(){
+  let image=document.getElementById("undraw")
+  image.style.backgroundColor="red";
+  image.style.transition="all 4s ease-in-out"
+
+let display=document.getElementById("display")
+  display.style.height="400px";
+  display.style.width="400px";
+  display.style.transition="all 4s ease-in-out"
+} 
+function ClickMe(){
+let button=document.getElementById("btn")
+button.style.padding="5px 8px";
+button.style.border="2px solid grey";
+button.style.backgroundColor="skyblue";
+button.style.borderRadius="4px";
+button.style.color="white";
+button.style.fontWeight="400"
+}
+
+//     function onMouseOver(){
+// alert("mouse over");
+//     }
+//     function onMouseOut(){
+//       alert("mouse out");
+//     }
